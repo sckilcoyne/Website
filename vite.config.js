8c32b52b3c285ddc5652c58d4daf0bb5cf43caef
sckilcoyne <64379138+sckilcoyne@users.noreply.github.com>
2025-10-14 21:40:32 -0400

    clean up dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
+// import mdx from '@mdx-js/rollup'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Website/', // for deploying to repo.github.io, comment out for labs.bcu.org
   plugins: [
-    {enforce: 'pre', ...mdx({/* jsxImportSource: …, otherOptions… */})},
-    react({include: /\.(jsx|js|mdx|md|tsx|ts)$/})
+    // {enforce: 'pre', ...mdx({/* jsxImportSource: …, otherOptions… */})},
+    // react({include: /\.(jsx|js|mdx|md|tsx|ts)$/})
+    react()
   ]
 })
